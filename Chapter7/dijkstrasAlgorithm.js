c2dcca5c62bf84f8014f473860ef72e0dce63eb8
YuriNem <PsychoactivePie@gmail.com>
2019-05-22 09:04:21 +0300

    Added dijkstrasAlgorithm

---
+++ Chapter7/dijkstrasAlgorithm.js
@@ -0,0 +1,68 @@
+const graph = {
+    start: {
+        a: 6,
+        b: 2,
+    },
+    a: {
+        fin: 1,
+    },
+    b: {
+        a: 3,
+        fin: 5,
+    },
+    fin: {},
+};
+
+const costs = {
+    a: 6,
+    b: 2,
+    fin: Infinity,
+};
+
+const parents = {
+    a: 'start',
+    b: 'start',
+    fin: null,
+};
+
+const processed = [];
+
+const findLowestCostNode = costs => {
+    let lowestCost = Infinity;
+    let lowestCostNode = null;
+
+    for (const node in costs) {
+        cost = costs[node];
+
+        if (cost < lowestCost && !processed.includes(node)) {
+            lowestCost = cost;
+            lowestCostNode = node;
+        }
+    }
+
+    return lowestCostNode;
+};
+
+const dijkstrasAlgorithm = () => {
+    let node = findLowestCostNode(costs);
+
+    while (node !== null) {
+        const cost = costs[node];
+        const neighbors = graph[node];
+
+        for (const n of Object.keys(neighbors)) {
+            const newCost = cost + neighbors[n];
+
+            if (costs[n] > newCost) {
+                costs[n] = newCost;
+                parents[n] = node;
+            }
+        }
+
+        processed.push(node);
+        node = findLowestCostNode(costs);
+    }
+};
+
+dijkstrasAlgorithm();
+console.log(costs, parents);
